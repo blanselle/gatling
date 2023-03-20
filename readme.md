e4b09d00171ea99eae44c8bf1ab5681563be3a67
Benoit Lanselle <blanselle@exotec-blanselle.localdomain>
2023-03-20 09:36:03 +0100

    chore(architecture) add results folder

--- readme.md
+++ readme.md
@@ -13,7 +13,15 @@ cd ~
 git clone git@github.com:blanselle/gatling.git
 ```
 
-### Step 2: run gatling
+### Step 2: Edit scenario
+
+Replace XXX values in SimulationsTest.scala
+
+### Step 3: run gatling
 ```shell
 docker run -it --rm -v /home/blanselle/Projects/Exotec/gatling/conf:/opt/gatling/conf -v /home/blanselle/Projects/Exotec/gatling/user-files:/opt/gatling/user-files -v /home/blanselle/Projects/Exotec/gatling/results:/opt/gatling/results denvazh/gatling
 ```
+
+### Step 4: Analyze report
+
+Open idex.html file in new folder of results folder
--- user-files/simulations/SimulationsTest.scala
+++ user-files/simulations/SimulationsTest.scala
@@ -4,21 +4,21 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class SimulationsUpdateAlarms extends Simulation {
+class SimulationsTest extends Simulation {
 
   val httpConf = http
-    .baseUrl("https://preprod-controlcenter.exotec.com")
+    .baseUrl("XXX")
 
   val sentHeaders = Map(
-    "Authorization" -> "Basic V0NTIERldmVsb3BtZW50OllsYTVHaXNreFU2dkhFVE9Nd3BXK2dDb3liU0xRRmhtdFg7RCkodT1CTlAmSkEzcg==",
+    "Authorization" -> "Basic XXX",
     "Content-Type" -> "application/json"
   )
 
   val scn1 = scenario("Scenario1")
     .exec(
         http("Dashboard")
-        .post("/external-api/alarm")
-        .body(StringBody("""{"type":"SQL","output":[{"site":"WCS Development","value":2,"code":"TESTALARM","detail":"SystemStatus:Operational","record_at":"2023-02-15 15:13:00","occurred_at":"2023-02-15 15:13:00"}]}"""))
+        .post("XXX")
+        .body(StringBody("""{"XXX":"XXX"}"""))
         .headers(sentHeaders)
         .check(status.is(200))
     )
